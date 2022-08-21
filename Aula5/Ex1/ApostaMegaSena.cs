caca0a65d3fa1b6e07c09511dcb86f43aeb16888
Claudio Ildefonso <claudio.ildefonso@aluno.ifsp.edu.br>
2022-08-21 18:05:51 -0300

    commit inicial aula 5

---
+++ Aula5/Ex1/ApostaMegaSena.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class ApostaMegaSena
+    {
+        int[] aposta=new int[10];
+        int[] resultado = new int[6];
+        int contVitoria = 0;
+        public void leAposta()
+        {
+            int i = 0;
+            Console.WriteLine("Digite os 10 números da aposta: ");
+            while(i<10)
+            {
+                int numAposta = Int32.Parse(Console.ReadLine());
+                for(int j = 0; j < 9; j++) // verifica repetição
+                {
+                    if(numAposta == aposta[j])
+                    {
+                        int verifRepet = 0;
+                        while(verifRepet < 1)
+                        {
+                            Console.WriteLine("Digite um número diferente do que já digitou!");
+                            numAposta = Int32.Parse(Console.ReadLine());
+                            if(numAposta != aposta[j])
+                            {
+                                verifRepet++;
+                            }
+                        }
+                    }
+                    j++;
+                }
+                while (numAposta < 0 || numAposta > 60)
+                {
+                    Console.WriteLine("O número da aposta deve estar entre 1 e 60!!!");
+                    numAposta = Int32.Parse(Console.ReadLine());
+                }
+                
+                aposta[i] = numAposta;
+                i++;
+
+            }
+        }
+        public void apuraResultado(int[]resultado)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                int a = 0;
+                while (a < 6)
+                {
+                    if (resultado[a] == aposta[i])
+                    {
+                        contVitoria++;
+                    }
+                    a++;
+                }
+                i++;
+            }
+        }
+        public void mostraResultado()
+        {
+            if(contVitoria == 6)
+            {
+                Console.WriteLine("PARABÉNS VOCÊ ACERTOU {0} NÚMEROS E GANHOU :)", contVitoria) ; 
+            }
+            else
+            {
+                Console.WriteLine("INFELIZMENTE VOCÊ ACERTOU SOMENTE {0} E NÃO GANHOU :(", contVitoria);
+            }
+        }
+
+
+    }
+}
---
+++ Aula5/Ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] resultado = new int[6];
+            for(int i = 0;i<resultado.Length;i++)
+            {
+                Console.WriteLine("Digite o resultado do sorteio: ");
+            }
+
+            ApostaMegaSena aposta1 = new ApostaMegaSena();
+            aposta1.leAposta();
+            aposta1.apuraResultado(resultado);
+            aposta1.mostraResultado();
+
+            for (int i = 0; i < resultado.Length; i++)
+            {
+                Console.WriteLine(resultado[i]);
+            }
+        }
+    }
+}
