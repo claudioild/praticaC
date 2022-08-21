57acf57ab7cfde3cfdf583065636153899e1b033
Claudio Ildefonso <claudio.ildefonso@aluno.ifsp.edu.br>
2022-08-21 18:25:09 -0300

    funcionando todos os metodos-resultado manual

--- Aula5/Ex1/Program.cs
+++ Aula5/Ex1/Program.cs
@@ -11,20 +11,44 @@ namespace Ex1
         static void Main(string[] args)
         {
             int[] resultado = new int[6];
-            for(int i = 0;i<resultado.Length;i++)
+            int contResult = 0;
+            int parada=0;
+            Console.WriteLine("Digite o resultado do sorteio: ");
+            while(contResult<6)
             {
-                Console.WriteLine("Digite o resultado do sorteio: ");
+                int numAposta = Int32.Parse(Console.ReadLine());
+                for(int j = 0; j < 6; j++) // verifica repetição
+                {
+                    if(numAposta == resultado[j])
+                    {
+                        int verifRepet = 0;
+                        while(verifRepet < 1)
+                        {
+                            Console.WriteLine("Digite um número diferente do que já digitou!");
+                            numAposta = Int32.Parse(Console.ReadLine());
+                            if(numAposta != resultado[j])
+                            {
+                                verifRepet++;
+                            }
+                        }
+                    }
+                    j++;
+                }
+                while (numAposta < 0 || numAposta > 60) // verifica se o num atende os requisitos
+                {
+                    Console.WriteLine("O número da aposta deve estar entre 1 e 60!!!");
+                    numAposta = Int32.Parse(Console.ReadLine());
+                }
+                
+                resultado[contResult] = numAposta;
+                contResult++;
             }
-
             ApostaMegaSena aposta1 = new ApostaMegaSena();
             aposta1.leAposta();
             aposta1.apuraResultado(resultado);
             aposta1.mostraResultado();
 
-            for (int i = 0; i < resultado.Length; i++)
-            {
-                Console.WriteLine(resultado[i]);
-            }
+            parada = Int32.Parse(Console.ReadLine());
         }
     }
 }
